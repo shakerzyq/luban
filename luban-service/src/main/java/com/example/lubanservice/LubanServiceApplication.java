75b0eed01fe3fe9399590d4542a81808fa0272b9
zhouyangqing <1312197142@qq.com>
2024-04-08 19:26:06 +0800

    第一次提交

---
+++ luban-service/src/main/java/com/example/lubanservice/LubanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lubanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LubanServiceApplication.class, args);
+    }
+
+}
---
+++ luban-service/src/test/java/com/example/lubanservice/LubanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lubanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
